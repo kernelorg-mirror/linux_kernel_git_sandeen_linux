36961af66d6345933b7291a58eefe80bdca4ce63
Eric Sandeen <sandeen@redhat.com>
2025-03-03 10:11:50 -0600

    f2fs: make LAZYTIME a mount option flag
    Set LAZYTIME into sbi during parsing, and transfer it to the sb in
    fill_super, so that an sb is not required during option parsing.
    
    (Note: While lazytime is normally handled via mount flag in the vfs,
    some f2fs users do expect to be able to use it as an explicit mount
    option string via the mount syscall, so this option must remain.)
    
    Signed-off-by: Eric Sandeen <sandeen@redhat.com>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -115,6 +115,11 @@ extern const char *f2fs_fault_name[FAULT_MAX];
 #define F2FS_MOUNT_COMPRESS_CACHE	0x04000000
 #define F2FS_MOUNT_AGE_EXTENT_CACHE	0x08000000
 #define F2FS_MOUNT_INLINECRYPT		0x10000000
+/*
+ * Some f2fs environments expect to be able to pass the "lazytime" option
+ * string rather than using the MS_LAZYTIME flag, so this must remain.
+ */
+#define F2FS_MOUNT_LAZYTIME		0x20000000
 
 #define F2FS_OPTION(sbi)	((sbi)->mount_opt)
 #define clear_opt(sbi, option)	(F2FS_OPTION(sbi).opt &= ~F2FS_MOUNT_##option)
